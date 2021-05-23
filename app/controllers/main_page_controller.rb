650f40b9321553796b8ebf262bc97846f724068c
EhsanKhalid123 <s3838975@student.rmit.edu.au>
2021-05-23 17:44:52 +0000

    Heroku Deployment

--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -14,26 +14,12 @@ class MainPageController < ApplicationController
     $newsletterEmail = @newsletterEmail
 
     # UserNotifierMailer.send_signup_email(@user).deliver
-    UserNotifierMailer.send_signup_email(@newsletterEmail).deliver
+    # UserNotifierMailer.send_signup_email(@newsletterEmail).deliver
 
   end
 
   def home
-    session[:saved_list] ||= []
-    saved_list = Set.new(session[:saved_list])
-    @random_unsaved_product = Product::PRODUCTS.values.filter { |product| !saved_list.include? product[:id] }.sample
 
-    @count = Product::PRODUCTS.values.filter.count { |product| saved_list.include? product[:id] }
-
-    @name = params[:visitor_name]
-
-    @newsletterEmail = params[:newsletterEmail]
-    @success = 'Email Has Been Sent Successfully To: '
-
-    $newsletterEmail = @newsletterEmail
-
-    # UserNotifierMailer.send_signup_email(@user).deliver
-    UserNotifierMailer.send_signup_email(@newsletterEmail).deliver
   end
 
 
